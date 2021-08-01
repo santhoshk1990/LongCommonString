44c433fd60b4200373babd25968e382039b0f895
admin <admin@192.168.1.10>
2021-08-01 17:43:34 +0530

    Exception Page Added

--- lcsProject/src/main/java/com/spring/lcs/controller/HomeController.java
+++ lcsProject/src/main/java/com/spring/lcs/controller/HomeController.java
@@ -7,21 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.spring.lcs.exception.SimilarStringException;
 import com.spring.lcs.service.FetchService;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = "/findLcs")
-	public ModelAndView getLCS(@RequestParam("string1") String str1, @RequestParam("string2") String str2, @RequestParam("string3") String str3, HttpServletRequest request, HttpServletResponse response) throws SimilarStringException
+	public ModelAndView getLCS(@RequestParam("string1") String str1, @RequestParam("string2") String str2, @RequestParam("string3") String str3, HttpServletRequest request, HttpServletResponse response) 
 	{
 				
 		if(str1.equals(str2) || str1.equals(str3) || str2.equals(str3))
 			
-			throw new SimilarStringException("Same String Passed. Please Pass Unique Strings");
-			
+			return new ModelAndView("exception");			
 		else 
 		{
 				
