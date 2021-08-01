a8a229b7029f6b8a8a81d334b0aee7f13a7f1238
admin <admin@192.168.1.10>
2021-08-01 23:13:47 +0530

    LCS changes

--- lcsProject/src/main/java/com/spring/lcs/controller/HomeController.java
+++ lcsProject/src/main/java/com/spring/lcs/controller/HomeController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.spring.lcs.service.FetchService;
@@ -12,7 +13,8 @@ import com.spring.lcs.service.FetchService;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "/findLcs")
+	
+	@RequestMapping(value = "/findLcs", method = RequestMethod.POST, produces = "application/json")
 	public ModelAndView getLCS(@RequestParam("string1") String str1, @RequestParam("string2") String str2, @RequestParam("string3") String str3, HttpServletRequest request, HttpServletResponse response) 
 	{
 				
--- lcsProject/src/main/webapp/WEB-INF/views/index.jsp
+++ lcsProject/src/main/webapp/WEB-INF/views/index.jsp
@@ -19,5 +19,34 @@
 		<input type="text" name="string3"><br>
 		<input type="submit">
 	</form>
+	
+	
+	<hr/>
+	<div id="displayDiv" style="display:none">
+		<div id="processedData"></div>
+	</div>
+    <script>
+	jQuery(document).ready(function($) {
+ 
+		$("#submit").click(function(){
+			var sdata = {};
+			sdata["string1"] = $("string1").val();
+			sdata["string2"] = $("string2").val();
+			sdata["string3"] = $("string3").val();
+			$.ajax({
+				type : "POST",
+				contentType : "application/json",
+				url : "findLCS",
+				data : JSON.stringify(data),
+				dataType : 'json',				
+				success : function(data) {
+					$('#processedData').html(JSON.stringify(data));
+					$('#displayDiv').show();
+				}
+			});
+		});
+ 
+	});
+    </script>	
 </body>
 </html>
